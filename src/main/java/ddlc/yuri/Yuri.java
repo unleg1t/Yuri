060a6d60c7db92aa3fd521bb76e5b88825bef015
unlegit <143151109+unleg1t@users.noreply.github.com>
2026-09-17 19:20:54 -0500

    chore: update client to 1.3.0

--- src/main/java/ddlc/yuri/Yuri.java
+++ src/main/java/ddlc/yuri/Yuri.java
@@ -22,7 +22,7 @@ public class Yuri {
     public static final Yuri INSTANCE = new Yuri();
     public static final String NAME = "Yuri";
     public static final String BUILD = "Beta";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
     public static final String FULL = NAME + " " + VERSION + " " + BUILD;
 
     private EventBus eventBus;
